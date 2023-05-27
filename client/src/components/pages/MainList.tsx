aacade15936f253e552c84cf2ad342562b5d3409
bini59 <myuni0234@gmail.com>
2023-05-27 23:09:01 +0900

    chore : 테스트 데이터 입력
    book list에서 테스트 데이터 입력

--- client/src/components/pages/MainList.tsx
+++ client/src/components/pages/MainList.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 
+
+import Book from "../Book";
+
 interface MainListProps {
     tag: string;
 }
 
 const MainList = ({ tag }: MainListProps) => {
+    const temp:book = {"title":"테스트","description":"테스트","url":"https://via.placeholder.com/150", "id":1, "author": "홍길동"};
+
     return (
         <div>
-        <h1>{tag}</h1>
+            <h1>{tag}</h1>
+            <Book book={temp} />
         </div>
     );
 }

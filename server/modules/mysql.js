ae930fe7b21b066e9387ed828f0c1503de46adbe
bini59 <myuni0234@gmail.com>
2023-05-28 02:29:45 +0900

    fix : mysql 연결 설정

--- server/modules/mysql.js
+++ server/modules/mysql.js
@@ -2,6 +2,7 @@ const mysql = require('mysql');
 const config = require('../config/auth');
 
 const connection = mysql.createConnection(config);
+connection.connect();
 console.log("mysql connection");
 
 module.exports = connection;
